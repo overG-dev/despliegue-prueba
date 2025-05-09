c3398921ce31c88e772824cbe555ee3c0789b667
Estefano-Lostaunau <u202211742@upc.edu.pe>
2025-05-09 08:31:39 -0500

    build

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  // In real lifecycles, this file should not be part of version control
+  production: true,
+  // Server Base URL for REST API
+  serverBaseUrl: 'https://681dfb44c1c291fa66327b98.mockapi.io/api/v1',
+  coursesEndpointPath: '/courses',
+};
